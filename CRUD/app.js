dee55413436e177732cd4bf68b00c8e0eef868c5
Julio Diaz <diaz.cesar.julio@gmail.com>
2020-03-22 18:03:09 -0400

    Agregando elementos

--- CRUD/app.js
+++ CRUD/app.js
@@ -1,16 +1,25 @@
-Vue.component('crud-component',{
-    props:{
-        //
-    },  
-    template: ``,
-    methods:{
-        //
-    }
-})
-
 var myApp = new Vue({
     el: "#app",
     data:{
-        //
-    }
+        clave:'',
+        titulo:'',
+        descripcion:'',
+        lista:[],
+    },
+    methods:{
+        agregar: function(clave, titulo, descripcion) {
+            var item = {
+                clave: clave,
+                titulo: titulo,
+                descripcion: descripcion,
+            }
+            this.lista.push(item)
+            this.limpiarCampos()
+        },
+        limpiarCampos: function() {
+            this.clave = ""
+            this.titulo =  ""
+            this.descripcion = ""
+        },
+    },
 });
