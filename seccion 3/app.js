892f7157da31005da1fe3cfdff79dacaa6de22d6
Julio Diaz <diaz.cesar.julio@gmail.com>
2020-03-22 13:35:26 -0400

    Utilizando methods en el componente

--- seccion 3/app.js
+++ seccion 3/app.js
@@ -1,15 +1,28 @@
 Vue.component('mi-primer-componente',{
-    props:[
-        'msg'
-    ],
+    props:{
+        msg:{
+            type: String,
+            required: true,
+        }
+    },  
     template: `
         <div class="alert alert-success" role="alert">
             Este es mi componente 
-            <button class="btn btn-primary">
+            <button class="btn btn-primary" @click="saludar()">
                 {{ msg }}
             </button>
-        </div>`
+        </div>`,
+    methods:{
+        saludar:function() {
+            alert("Hola " + this.msg);
+        }
+    }
 })
+
 var myApp = new Vue({
     el: "#app",
+    data:{
+        Enviar: "Enviando...",
+        Jugar: "Jugando...",
+    }
 });
