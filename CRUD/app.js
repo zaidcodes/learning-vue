463ef25c134faa275ad0c9a05e88c9251c5c3ae6
Julio Diaz <diaz.cesar.julio@gmail.com>
2020-03-22 13:38:36 -0400

    crud inicial

---
+++ CRUD/app.js
@@ -0,0 +1,16 @@
+Vue.component('crud-component',{
+    props:{
+        //
+    },  
+    template: ``,
+    methods:{
+        //
+    }
+})
+
+var myApp = new Vue({
+    el: "#app",
+    data:{
+        //
+    }
+});
