7c2dc49d9b63a6f126d673da8d5d81b8a7f5a7f5
Julio Diaz <diaz.cesar.julio@gmail.com>
2020-03-22 11:19:35 -0400

    Computed properties + v-model

--- Seccion 2/js/app.js
+++ Seccion 2/js/app.js
@@ -2,10 +2,18 @@ var myApp = new Vue({
     el: "#app",
     data: {
         votos: 0,
+        nombre: "",
+        apellido: "",
     },
     methods: {
         votar: function (){
             this.votos++;
+            
         }
+    },
+    computed: {
+        nombreCompleto: function(){
+            return this.nombre + " " + this.apellido;
+        },
     }
 });
